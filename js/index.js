6fda90925bca4ce3cbdafc061ffbd710ce30ee01
Evgenia4444 <Pochikyan4@yandex.ru>
2021-06-04 08:32:16 +0300

    u

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".nav_btn").click(function(){
+      $('.modal').toggleClass("pink");
+      
+    });
+
+});
---
+++ js/shop.js
@@ -0,0 +1,72 @@
+let slidePosition = 0;
+const slides = document.getElementsByClassName('carousel_item');
+const totalSlides = slides.length;
+
+
+$('.carousel_button-next').on("click", function () {
+    moveToNextSlide();
+});
+
+
+$('.carousel_button-prev').on("click", function () {
+    moveToPrevSlide();
+});
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel_item-visible');
+        slide.classList.add('carousel_item-hidden');
+    }
+
+    slides[slidePosition].classList.add('carousel_item-visible');
+}
+
+function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+
+    updateSlidePosition();
+}
+
+
+
+    // $(document).ready(function() {
+    //     $('.carousel_button-prev').click(function(){
+    //         if(slidePosition === 0){
+    //             slidePosition = 0;
+    //         } else {
+    //             slidePosition--;
+    //         }
+    //         $('.carousel_item').removeClass('carousel_item-visible');
+    //         $('.carousel_item').addClass('carousel_item-hidden');
+    //     })
+    // })
+
+    // $(document).ready(function() {
+    //     $('.carousel_button-next').click(function(){
+    //         if(slidePosition === totalSlides - 1){
+    //             slidePosition = 0;
+    //         } else {
+    //             slidePosition++;
+    //         }
+    //         $('.carousel_item').removeClass('carousel_item-visible');
+    //         $('.carousel_item').addClass('carousel_item-hidden');
+    //     })
+    // })
+
+    // function moveToNextSlide(){
+    //     console.log('hey')
+    // }
