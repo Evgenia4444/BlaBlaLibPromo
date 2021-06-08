84ed4b1a9b75a67543e84d66ce94bce29b66da83
Evgenia4444 <Pochikyan4@yandex.ru>
2021-06-08 17:17:57 +0300

    t

--- js/znamenitosti.js
+++ js/znamenitosti.js
@@ -1,12 +1,5 @@
-$(".person_card").hover(function () {
+$(".person_card").hover( function () {
     
-    $('.person_card').css({
-        "border": "7px solid #fff",
-        'border-radius': '20px 0 0 0'
-        
-    });
-    $('.person_card-bottom').css({
-        'bottom': '-50px',
-        'opacity': '1'
-    })
+    $(this).toggleClass('cardOnHover');
+    $('.person_card-bottom').toggleClass('cardBottomOnHover');
 });
