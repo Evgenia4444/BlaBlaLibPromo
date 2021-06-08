a1668d8e1ed3fa9f16cd0bdaa5d6113f78acac4f
Evgenia4444 <Pochikyan4@yandex.ru>
2021-06-08 15:12:44 +0300

    6

---
+++ js/znamenitosti.js
@@ -0,0 +1,11 @@
+$(".person_card").hover(function () {
+    $('.person_card').css({
+        "border": "7px solid #fff",
+        'border-radius': '20px 0 0 0'
+        
+    });
+    $('.person_card-bottom').css({
+        'bottom': '-50px',
+        'opacity': '1'
+    })
+});
