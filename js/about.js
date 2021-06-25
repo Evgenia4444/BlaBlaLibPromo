635c91be5614a1707344507d0c99eefc45374122
Evgenia4444 <Pochikyan4@yandex.ru>
2021-06-25 03:20:38 +0300

    7

--- js/about.js
+++ js/about.js
@@ -2,8 +2,10 @@ $('.burgerMenu').click(function() {
     $('.mobileMenu').toggleClass('menuVisible')
   })
 
-  $('.multiple-items').slick({
+  $('.slider').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3
   });
+
+  
