baad9fa861fd00940d26d97358fa6c1a79df2b91
Evgenia4444 <Pochikyan4@yandex.ru>
2021-06-25 12:35:00 +0300

    u

--- js/znamenitosti.js
+++ js/znamenitosti.js
@@ -16,6 +16,29 @@ $('.vangogh').click(function(){
     $('.vangogh_open').toggleClass('visible')
 })
 
+$('.klodmone').click(function(){
+    $('.klodmone_open').toggleClass('visible')
+})
+
+$('.edgardega').click(function(){
+    $('.edgardega_open').toggleClass('visible')
+})
+
+$('.salvador').click(function(){
+    $('.salvador_open').toggleClass('visible')
+})
+$('.frida').click(function(){
+    $('.frida_open').toggleClass('visible')
+})
+$('.freddie').click(function(){
+    $('.freddie_open').toggleClass('visible')
+})
+$('.bethoven').click(function(){
+    $('.bethoven_open').toggleClass('visible')
+})
+
+
+
 $('.burgerMenu').click(function() {
     $('.mobileMenu').toggleClass('menuVisible')
 })
