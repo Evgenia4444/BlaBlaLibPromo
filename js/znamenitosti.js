6d38e27b5f40ccf4fa6e23c2cef08d27df80d425
Evgenia4444 <Pochikyan4@yandex.ru>
2021-06-25 13:05:14 +0300

    е

--- js/znamenitosti.js
+++ js/znamenitosti.js
@@ -37,6 +37,58 @@ $('.bethoven').click(function(){
     $('.bethoven_open').toggleClass('visible')
 })
 
+$('.amy').click(function(){
+    $('.amy_open').addClass('visible')
+})
+$('.michael').click(function(){
+    $('.michael_open').addClass('visible')
+})
+$('.ferenc').click(function(){
+    $('.ferenc_open').addClass('visible')
+})
+$('.ernest').click(function(){
+    $('.ernest_open').addClass('visible')
+})
+$('.julesverne').click(function(){
+    $('.julesverne_open').addClass('visible')
+})
+$('.dostoevski').click(function(){
+    $('.dostoevski_open').addClass('visible')
+})
+$('.arthur').click(function(){
+    $('.arthur_open').addClass('visible')
+})
+$('.lincoln').click(function(){
+    $('.lincoln_open').addClass('visible')
+})
+
+
+
+
+
+
+
+
+
+
+
+$('.closeIcon').click(function(){
+    $('.person_card-open').removeClass('visible')
+})
+
+$('.whiteCloseIcon').click(function(){
+    $('.cardSuccess').css({
+        'opacity': '0'
+    })
+})
+
+
+$('.card_btn').click(function(){
+    $('.cardSuccess').css({
+        'opacity': '1'
+    })
+})
+
 
 
 $('.burgerMenu').click(function() {
